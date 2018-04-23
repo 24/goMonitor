588f526b27e893e5dc7257c40c089d2455db1c2f
lloooo <409023726@qq.com>
2018-04-23 23:59:59 +0800

    support webview

--- main.go
+++ main.go
@@ -8,20 +8,19 @@ import (
 	"regexp"
 	"strings"
 	"time"
+	"log"
+	"os"
 )
 
 const url = "https://www.zuanke8.com/zuixin.php"
-const delimiter  = "-------------分割线------------"
 
 func main() {
-	for {
-		data, statusCode := Get(url)
-		if statusCode != 200 {
-			fmt.Println(statusCode)
-		}
-		parseData(data)
-		println(delimiter)
-		time.Sleep(5*time.Second)
+	argCount := len(os.Args[1:])
+	if argCount > 0 {
+		cmd()
+	} else {
+		http.HandleFunc("/zuanke8", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 	}
 }
 
@@ -51,9 +50,10 @@ func ConvertToString(src string, srcCode string, tagCode string) string {
 	return result
 }
 
-func parseData(data string) {
+func parseData(data string) []map[string]string {
 	reg := regexp.MustCompile(`<a href="https://www.zuanke8.com/thread.*</a>`)
 	result := reg.FindAllStringSubmatch(data, 1000)
+	retArr := make([]map[string]string, 0)
 	for _, item := range result {
 		titleReg := regexp.MustCompile(`title.*target`)
 		title := titleReg.FindString(item[0])
@@ -63,9 +63,41 @@ func parseData(data string) {
 		title = strings.TrimRight(title, `"  target`)
 		match, _ := regexp.MatchString("(速度|水|快|好价|还款)", title)
 		if match {
-			fmt.Println(title)
-			fmt.Println(url)
-			fmt.Println()
+			retItem := make(map[string]string)
+			retItem["title"] = title
+			retItem["url"] = url
+			retArr = append(retArr, retItem)
 		}
 	}
+
+	return retArr
+}
+
+func cmd() {
+	for {
+		data, statusCode := Get(url)
+		if statusCode != 200 {
+			fmt.Println(statusCode)
+		}
+		retArr := parseData(data)
+		for _, Item := range retArr {
+			fmt.Printf("%s\n%s\n\n", Item["title"], Item["url"])
+		}
+		println()
+		println()
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	data, statusCode := Get(url)
+	if statusCode != 200 {
+		fmt.Println(statusCode)
+	}
+	retArr := parseData(data)
+	fmt.Fprintf(w, "%v", `<!DOCTYPE HTML><html> <meta http-equiv="refresh" content="1"><body>`)
+	for _, item := range retArr {
+		fmt.Fprintf(w, `<a href="%s" target="_blank">%s</a><br> `, item["url"], item["title"])
+	}
+	fmt.Fprintf(w, "%v", `</body></html>`)
 }
