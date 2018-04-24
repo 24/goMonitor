3e888f3cc8cc30bdc8692d3653379481d5c19e2c
lloooo <409023726@qq.com>
2018-04-24 17:01:09 +0800

    finish zuanke8

--- main.go
+++ main.go
@@ -4,24 +4,73 @@ import (
 	"net/http"
 	"io/ioutil"
 	"fmt"
-	"github.com/axgle/mahonia"
-	"regexp"
 	"strings"
 	"time"
 	"log"
 	"os"
+	"encoding/json"
+	"regexp"
+	"./src/userUtil"
 )
-
-const url = "https://www.zuanke8.com/zuixin.php"
 
 func main() {
 	argCount := len(os.Args[1:])
 	if argCount > 0 {
 		cmd()
 	} else {
-		http.HandleFunc("/zuanke8", handler)
+		http.HandleFunc("/", handler)
 		log.Fatal(http.ListenAndServe("localhost:8000", nil))
 	}
+}
+
+func zuanke8RawData() (content string, statusCode int) {
+	zuanke8url := "http://www.zuanke8.com/api/mobile/index.php?sessionid=&version=4.1&zstamp=" + "1524549400" + "&module=zuixin&sign=" + "16f179de310d945c7f17d91d3e97b8e4"
+	req, err := http.NewRequest("POST", zuanke8url, strings.NewReader("mod=all"))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		statusCode = -1
+		return
+	}
+	client := &http.Client{}
+	response, err := client.Do(req)
+	defer response.Body.Close()
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		statusCode = -2
+		return
+	}
+	statusCode = response.StatusCode
+	content = userUtil.ConvertToString(string(data), "gbk", "utf-8")
+	return content, statusCode
+}
+
+func parseZuanke8Data(data string) []map[string]string {
+	type Relist []struct {
+		Subject string `json:"subject"`
+		Tid     string `json:"tid"`
+	}
+	type Data struct {
+		Relist Relist
+	}
+	type Result struct {
+		Data Data `json:"data"`
+	}
+	var result Result
+	if err := json.Unmarshal([]byte(data), &result); err != nil {
+		log.Fatal("json unmarsha1 failed", err)
+	}
+	retArr := make([]map[string]string, 0)
+	for _, item := range result.Data.Relist {
+		match, _ := regexp.MatchString("(速度|水|快|好价|还款)", item.Subject)
+		if match {
+			retItem := make(map[string]string)
+			retItem["title"] = item.Subject
+			retItem["zuanke8url"] = "https://www.zuanke8.com/thread-" + item.Tid + "-1-1.html"
+			retArr = append(retArr, retItem)
+		}
+	}
+
+	return retArr
 }
 
 func Get(url string) (content string, statusCode int) {
@@ -37,51 +86,19 @@ func Get(url string) (content string, statusCode int) {
 		return
 	}
 	statusCode = response.StatusCode
-	content = ConvertToString(string(data), "gbk", "utf-8")
+	content = userUtil.ConvertToString(string(data), "gbk", "utf-8")
 	return
-}
-
-func ConvertToString(src string, srcCode string, tagCode string) string {
-	srcCoder := mahonia.NewDecoder(srcCode)
-	srcResult := srcCoder.ConvertString(src)
-	tagCoder := mahonia.NewDecoder(tagCode)
-	_, cdata, _ := tagCoder.Translate([]byte(srcResult), true)
-	result := string(cdata)
-	return result
-}
-
-func parseData(data string) []map[string]string {
-	reg := regexp.MustCompile(`<a href="https://www.zuanke8.com/thread.*</a>`)
-	result := reg.FindAllStringSubmatch(data, 1000)
-	retArr := make([]map[string]string, 0)
-	for _, item := range result {
-		titleReg := regexp.MustCompile(`title.*target`)
-		title := titleReg.FindString(item[0])
-		urlReg := regexp.MustCompile(`https://.*html`)
-		url := urlReg.FindString(item[0])
-		title = strings.TrimLeft(title, `title="`)
-		title = strings.TrimRight(title, `"  target`)
-		match, _ := regexp.MatchString("(速度|水|快|好价|还款)", title)
-		if match {
-			retItem := make(map[string]string)
-			retItem["title"] = title
-			retItem["url"] = url
-			retArr = append(retArr, retItem)
-		}
-	}
-
-	return retArr
 }
 
 func cmd() {
 	for {
-		data, statusCode := Get(url)
+		data, statusCode := zuanke8RawData()
 		if statusCode != 200 {
 			fmt.Println(statusCode)
 		}
-		retArr := parseData(data)
+		retArr := parseZuanke8Data(data)
 		for _, Item := range retArr {
-			fmt.Printf("%s\n%s\n\n", Item["title"], Item["url"])
+			fmt.Printf("%s\n%s\n\n", Item["title"], Item["zuanke8url"])
 		}
 		println()
 		println()
@@ -90,14 +107,14 @@ func cmd() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	data, statusCode := Get(url)
+	data, statusCode := zuanke8RawData()
 	if statusCode != 200 {
 		fmt.Println(statusCode)
 	}
-	retArr := parseData(data)
+	retArr := parseZuanke8Data(data)
 	fmt.Fprintf(w, "%v", `<!DOCTYPE HTML><html> <meta http-equiv="refresh" content="5"><body>`)
 	for _, item := range retArr {
-		fmt.Fprintf(w, `<a href="%s" target="_blank">%s</a><br> `, item["url"], item["title"])
+		fmt.Fprintf(w, `<a href="%s" target="_blank">%s</a><br> `, item["zuanke8url"], item["title"])
 	}
 	fmt.Fprintf(w, "%v", `</body></html>`)
 }
